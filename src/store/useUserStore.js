433592a294d10e7170755725564edd1b4fa50923
thieutq <thieutq@example.com>
2024-08-17 09:59:43 +0700

    feat: add user store

---
+++ src/store/useUserStore.js
@@ -0,0 +1,12 @@
+import create from 'zustand';
+
+const useUserStore = create((set) => ({
+  users: [],
+  addAllUsers: (newUsers) => set((state) => ({ users: [...state.users, ...newUsers] })),
+  addUser: (newUser) => set((state) => ({ users: [...state.users, newUser] })),
+  removeUser: (userId) => set((state) => ({
+    users: state.users.filter((user) => user.id !== userId),
+  })),
+}));
+
+export default useUserStore;
